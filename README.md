cee5d7aaa35d4211db8dd3ff8a1e6bd6587299a1
冯祥卫 <1025139448@qq.com>
2019-09-21 22:49:03 +0800

    chinese_characters

---
+++ README.md
@@ -0,0 +1,2 @@
+# complicated_chinese_characters
+ 
---
+++ visualization.m
@@ -0,0 +1,16 @@
+%This data contains five Chinese characters with complicate structures, 
+%namely, 'cake', 'dim', 'math', 'micro', and 'tree'
+%There are three types of degenerations with five levels, which are  nonrigid deformation,
+%noise and outliers.
+clear;clc
+close all
+filepath='.\data\';
+i=5;%Level
+j=5;%Number
+load([filepath 'save_cake_def_' num2str(i) '_' num2str(j) '.mat'])
+% load([filepath 'save_cake_noise_' num2str(i) '_' num2str(j) '.mat'])
+% load([filepath 'save_cake_outlier_' num2str(i) '_' num2str(j) '.mat'])
+figure(1)
+plot(model_data(:,1),model_data(:,2),'bo',scene_data(:,1),scene_data(:,2),'r.')
+axis off
+drawnow
