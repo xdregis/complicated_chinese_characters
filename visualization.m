5a52b3af399eddec14299b772edadb632a7f8276
xdregis <55405406+xdregis@users.noreply.github.com>
2019-09-30 18:10:43 +0800

    Update visualization.m

--- visualization.m
+++ visualization.m
@@ -1,4 +1,4 @@
-%This data contains five Chinese characters with complicate structures, 
+%This data contains five Chinese characters with complicated structures, 
 %namely, 'cake', 'dim', 'math', 'micro', and 'tree'
 %There are three types of degenerations with five levels, which are  nonrigid deformation,
 %noise and outliers.
